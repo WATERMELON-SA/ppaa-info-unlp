271ee3ef2e702d7d5afbbd787de61b0e025ac98b
nachosoloaga <nachosoloaga@gmail.com>
2024-03-23 00:30:04 -0300

    Add TypeORM libs and example Swagger decorators

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../services/app.service';
-
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiResponse({ status: 200, description: 'Returns the hello message' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
