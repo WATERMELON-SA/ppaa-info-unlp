0989b265c9622f8dbf6ea2bd078ee81b478f92df
nachosoloaga <nachosoloaga@gmail.com>
2024-03-22 22:39:35 -0300

    Add Swagger and Prettier libs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('PPAA - Info UNLP')
+    .setVersion('0.0.1')
+    .addTag('ppaa')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
